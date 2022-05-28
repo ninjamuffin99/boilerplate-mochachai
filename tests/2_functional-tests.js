f58ce6743209692f640fb057cea1217208050c2c
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2022-05-27 22:01:46 -0400

    asserts woops

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -39,7 +39,10 @@ suite('Functional Tests', function() {
                 .send({ "surname": ["Colombo"] })
 
             .end(function(err, res) {
-                assert.fail();
+                assert.equal(res.status, 200);
+                assert.equal(res.type, "application/json");
+                assert.equal(res.body.name, "Cristoforo");
+                assert.equal(res.body.surname, "Colombo");
 
                 done();
             });
